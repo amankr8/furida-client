6fcad3f238b312a878eada2336729b69a39a9c0a
Aman Kumar <rx8aman@gmail.com>
2021-09-06 14:54:54 +0530

    update

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import App from './App'
 const store = createStore(
     reducers,
     compose(applyMiddleware(thunk))
+    // compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 
 ReactDOM.render(
