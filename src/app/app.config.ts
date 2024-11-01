5162c0468e69ac0adb039cd88972c6e3d6d877ce
amankr8 <amankumar141099@gmail.com>
2024-11-01 12:07:30 +0530

    interceptor fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 import { jwtInterceptor } from './jwt.interceptor';
 
@@ -11,9 +11,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
     { provide: JWT_OPTIONS, useValue: {} },
     { provide: JwtHelperService, useClass: JwtHelperService },
-    { provide: HTTP_INTERCEPTORS, useValue: jwtInterceptor, multi: true },
   ],
 };
--- src/app/jwt.interceptor.ts
+++ src/app/jwt.interceptor.ts
@@ -1,11 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log('Interceptor is called');
   const token = localStorage.getItem('jwtToken'); // Retrieve the JWT token from local storage
 
   if (token) {
-    console.log('Token: ', token);
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
