d0a4707d45eda29cd1642402f68d8b09dd3320b3
amankr8 <amankumar141099@gmail.com>
2024-11-02 17:18:46 +0530

    Auth guard enhanced

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,10 +1,12 @@
 import { inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CanActivateFn, Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const jwtHelper = inject(JwtHelperService);
   const router = inject(Router);
+  const snackBarRef = inject(MatSnackBar);
 
   // Retrieve the token from local storage
   const token = localStorage.getItem('jwtToken');
@@ -15,6 +17,9 @@ export const authGuard: CanActivateFn = (route, state) => {
   } else {
     // Redirect to the login page if the token is missing or expired
     router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    snackBarRef.open('Session expired! Please login again', 'Dismiss', {
+      duration: 3000,
+    });
     return false;
   }
 };
