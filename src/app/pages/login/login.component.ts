0194dce7827ed32860d08eaac034f49b00371d64
amankr8 <amankumar141099@gmail.com>
2024-11-02 18:02:26 +0530

    code optimized

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -42,7 +42,6 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    this.authService.logout();
     this.form = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', [
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -45,7 +45,6 @@ export class SignupComponent {
   ) {}
 
   ngOnInit() {
-    this.authService.logout();
     this.roleService.getAllRoles().subscribe((data) => {
       this.roles = data;
     });
