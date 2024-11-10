f9ed4174f64da51fb7999248aca840dc41dd216a
amankr8 <amankumar141099@gmail.com>
2024-11-10 20:52:38 +0530

    Added loading property in posts screen

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -11,16 +11,24 @@ import { Router } from '@angular/router';
 import { DeletePostComponent } from '../delete-post/delete-post.component';
 import { AuthService } from '../../service/auth.service';
 import { HeaderComponent } from '../../components/header/header.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CommonModule, HeaderComponent],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CommonModule,
+    HeaderComponent,
+    MatProgressBarModule,
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
 })
 export class PostsComponent {
   posts: Post[] = [];
+  isLoading = false;
   readonly editDialog = inject(MatDialog);
   readonly deleteDialog = inject(MatDialog);
   private snackBarRef = inject(MatSnackBar);
@@ -57,6 +65,7 @@ export class PostsComponent {
 
     editDialogref.afterClosed().subscribe((updatedPost: Post) => {
       if (!updatedPost) return;
+      this.isLoading = true;
 
       const postDto = new FormData();
       postDto.append('title', updatedPost.title);
@@ -67,6 +76,7 @@ export class PostsComponent {
             (post) => post.id === updatedPost.id
           );
           this.posts[index] = updatedPost;
+          this.isLoading = false;
           this.snackBarRef.open('Post updated successfully!', 'Dismiss', {
             duration: 3000,
           });
@@ -90,9 +100,11 @@ export class PostsComponent {
     deleteDialogref.afterClosed().subscribe({
       next: (res) => {
         if (!id) return;
+        this.isLoading = true;
 
         this.postService.deletePost(id).subscribe((res) => {
           this.posts = this.posts.filter((post) => post.id !== id);
+          this.isLoading = false;
           this.snackBarRef.open('Post deleted successfully!', 'Dismiss', {
             duration: 3000,
           });
