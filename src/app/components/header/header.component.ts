2499417c4faa27b74cdb1f9d6a2bd84112390fb0
amankr8 <amankumar141099@gmail.com>
2024-11-23 19:12:00 +0530

    Header refactored

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  constructor(private router: Router) {}
-
-  navigateToHome() {
-    this.router.navigate(['']);
-  }
-}
+export class HeaderComponent {}
