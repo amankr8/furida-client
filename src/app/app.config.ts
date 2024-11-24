3e9a162fe26a28ffcae6d6a5b6160b1113399e74
amankr8 <amankumar141099@gmail.com>
2024-11-24 21:51:44 +0530

    footer attached to bottom

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
-import { jwtInterceptor } from './jwt.interceptor';
+import { jwtInterceptor } from './config/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
