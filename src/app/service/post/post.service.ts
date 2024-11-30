cee012e4be1dd8299075559b6efece8e4b7dac97
amankr8 <amankumar141099@gmail.com>
2024-11-30 06:50:59 +0530

    errors resolved

--- src/app/service/post/post.service.ts
+++ src/app/service/post/post.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Post } from '../interface/post';
+import { Post } from '../../interface/post';
 
 @Injectable({
   providedIn: 'root',
