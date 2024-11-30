da8d4368c20b878b5203a63646c5dcaf15465c72
amankr8 <amankumar141099@gmail.com>
2024-11-30 11:39:34 +0530

    create project form created

--- src/app/pages/admin/projects/components/project-form/project-form.component.ts
+++ src/app/pages/admin/projects/components/project-form/project-form.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { ProjectService } from '../../../../../service/project/project.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    CommonModule,
+    MatCardModule,
+    MatProgressSpinner,
+  ],
   templateUrl: './project-form.component.html',
-  styleUrl: './project-form.component.scss'
+  styleUrl: './project-form.component.scss',
 })
 export class ProjectFormComponent {
+  form!: FormGroup;
+  isLoading = false;
+  private snackBarRef = inject(MatSnackBar);
 
+  constructor(private projectService: ProjectService, private router: Router) {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      desc: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+    });
+  }
+
+  submit() {
+    this.isLoading = true;
+    this.projectService.addProject(this.form.value).subscribe({
+      next: (res) => {
+        this.router.navigate(['/admin/projects']);
+        this.snackBarRef.open(res.message, 'Dismiss', {
+          duration: 3000,
+        });
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.snackBarRef.open('this.getErrorMessage(err)', 'Dismiss', {
+          duration: 3000,
+        });
+      },
+    });
+  }
 }
--- src/app/service/project/project.service.ts
+++ src/app/service/project/project.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Project } from '../../interface/project';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,10 @@ export class ProjectService {
   apiUrl = 'http://localhost:8080/projects';
 
   constructor(private http: HttpClient) {}
+
+  addProject(project: Project): Observable<any> {
+    return this.http.post(this.apiUrl, project);
+  }
 
   getAllProjects(): Observable<any> {
     return this.http.get(this.apiUrl);
