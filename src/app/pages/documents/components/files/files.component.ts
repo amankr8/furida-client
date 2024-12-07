3b9f0fe41fe5f547182586a050267e3b54a51daa
amankr8 <amankumar141099@gmail.com>
2024-12-07 20:44:45 +0530

    implemented project link to doc on client view

--- src/app/pages/documents/components/files/files.component.ts
+++ src/app/pages/documents/components/files/files.component.ts
@@ -5,6 +5,8 @@ import { DocumentService } from '../../../../service/document/document.service';
 import { CommonModule } from '@angular/common';
 import { Document } from '../../../../interface/document';
 import { MatButtonModule } from '@angular/material/button';
+import { Project } from '../../../../interface/project';
+import { ProjectService } from '../../../../service/project/project.service';
 
 @Component({
   selector: 'app-files',
@@ -15,15 +17,26 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class FilesComponent {
   docs: Document[] = [];
+  projects: Project[] = [];
   isLoading = false;
 
-  constructor(private documentService: DocumentService) {}
+  constructor(
+    private documentService: DocumentService,
+    private projectService: ProjectService
+  ) {}
 
   ngOnInit() {
     this.isLoading = true;
+    this.projectService.getAllProjects().subscribe((data) => {
+      this.projects = data;
+    });
     this.documentService.getAllDocuments().subscribe((data) => {
       this.docs = data;
       this.isLoading = false;
     });
+  }
+
+  getProjectNameById(id: number) {
+    return this.projects.find((project) => project.id === id)?.name;
   }
 }
