ed229a1c2856f8bb111dec4c40dc78c33e44f480
amankr8 <amankumar141099@gmail.com>
2024-12-14 15:28:40 +0530

    environment variable changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://furida-fab61ec7daec.herokuapp.com/', // Replace with your API base URL
+  baseUrl: 'http://localhost:8080/', // Replace with your API base URL
 };
