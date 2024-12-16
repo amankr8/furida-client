e6075afb4d2c116494bffdd2c54b4fcd9e384400
Aman Kumar <amankumar141099@gmail.com>
2024-12-17 00:11:09 +0530

    FWA-7 - EDIT and DELETE button does not work in users page

--- src/app/pages/admin/users/components/cards/cards.component.ts
+++ src/app/pages/admin/users/components/cards/cards.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { User } from '../../../../../interface/user';
-import { combineLatest, map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import {
   selectIsUserLoaded,
@@ -16,7 +16,7 @@ import {
   openDeleteDialog,
   openEditDialog,
 } from '../../../../../store/actions/user.actions';
-import { selectAuthUsername } from '../../../../../store/selectors/auth.selectors';
+import { AuthService } from '../../../../../service/auth/auth.service';
 
 @Component({
   selector: 'app-user-cards',
@@ -29,27 +29,19 @@ export class CardsComponent {
   users$: Observable<User[]> = this.store.select(selectUsers);
   isUserLoaded: Observable<boolean> = this.store.select(selectIsUserLoaded);
   loading$: Observable<boolean> = this.store.select(selectLoading);
-  loggedInUser$: Observable<string | null> =
-    this.store.select(selectAuthUsername);
+  loggedInUser: string | null = null;
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private authService: AuthService) {}
 
   ngOnInit() {
     this.isUserLoaded.subscribe((isLoaded) => {
       if (!isLoaded) this.store.dispatch(loadUsers());
     });
+    this.loggedInUser = this.authService.getLoggedInUsername();
   }
 
-  // Create an observable to track button states for each user
-  getButtonStates(
-    user: User
-  ): Observable<{ canEdit: boolean; canDelete: boolean }> {
-    return combineLatest([this.loggedInUser$, this.loading$]).pipe(
-      map(([loggedInUsername, loading]) => ({
-        canEdit: user.username === loggedInUsername && !loading,
-        canDelete: user.username !== loggedInUsername && !loading,
-      }))
-    );
+  isLoggedInUser(username: string): boolean {
+    return username === this.loggedInUser;
   }
 
   updateUser() {
