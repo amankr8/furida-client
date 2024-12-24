c836572b2253b9a0e4e2399c3def3d391ccbf720
Aman Kumar <amankumar141099@gmail.com>
2024-12-24 16:13:30 +0530

    FWA-33 - Fix mobile responsiveness in visitor facing screens

--- src/app/pages/home/components/banner-bottom/banner-bottom.component.ts
+++ src/app/pages/home/components/banner-bottom/banner-bottom.component.ts
@@ -1,14 +1,28 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-banner-bottom',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatDividerModule, CommonModule],
   templateUrl: './banner-bottom.component.html',
   styleUrl: './banner-bottom.component.scss',
 })
 export class BannerBottomComponent {
   email: string = 'furida.jsr@gmail.com';
   phone: string = '+91-7280069456';
+  smallScreen: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.XSmall])
+      .subscribe((result) => {
+        this.smallScreen = result.matches;
+      });
+  }
 }
