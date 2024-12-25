633afa4b53298c9c72c6afc286b9a9b2d5b5338a
Aman Kumar <amankumar141099@gmail.com>
2024-12-25 09:26:41 +0530

    FWA-36 - Projects button on mobile screen doesn't work

--- src/app/pages/home/components/side-menu/side-menu.component.ts
+++ src/app/pages/home/components/side-menu/side-menu.component.ts
@@ -4,6 +4,16 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { AdminButtonComponent } from '../admin-button/admin-button.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Observable } from 'rxjs';
+import { Project } from '../../../../interface/project';
+import { loadProjects } from '../../../../state/project/project.actions';
+import {
+  selectProjects,
+  selectIsProjectLoaded,
+} from '../../../../state/project/project.selectors';
+import { Store } from '@ngrx/store';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-menu',
@@ -14,12 +24,26 @@ import { AdminButtonComponent } from '../admin-button/admin-button.component';
     MatDividerModule,
     RouterLink,
     AdminButtonComponent,
+    MatExpansionModule,
+    CommonModule,
   ],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss',
 })
 export class SideMenuComponent {
   @Output() toggleSidenav = new EventEmitter<void>();
+  projects$: Observable<Project[]> = this.store.select(selectProjects);
+  projectLoaded$: Observable<boolean> = this.store.select(
+    selectIsProjectLoaded
+  );
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.projectLoaded$.subscribe((loaded) => {
+      if (!loaded) this.store.dispatch(loadProjects());
+    });
+  }
 
   toggleMenu() {
     this.toggleSidenav.emit();
