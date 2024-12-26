112ce3faeddcea8023ef10afaf4bbb94faf0157a
Aman Kumar <amankumar141099@gmail.com>
2024-12-26 09:59:30 +0530

    FWA-39 - Login form is not responsive

--- src/app/config/jwt.interceptor.ts
+++ src/app/config/jwt.interceptor.ts
@@ -1,23 +1,29 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthService } from '../service/auth/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { EMPTY } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const jwtHelper = inject(JwtHelperService);
   const router = inject(Router);
   const authService = inject(AuthService);
+  const snackBar = inject(MatSnackBar);
 
   const token = authService.getAuthToken();
 
   if (token) {
     if (jwtHelper.isTokenExpired(token)) {
-      alert('Your session has expired. Please log in again.');
+      snackBar.open('Your session has expired. Please log in again.', 'Close', {
+        duration: 3000,
+      });
       router.navigate(['/login']);
-      return next(req); // Prevents the request from being sent
+      return EMPTY; // Stop the request entirely
     }
 
+    // Attach Authorization header if token is valid
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
