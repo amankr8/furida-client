5bf18dc9b2bd4a2c25de866ae287ac2b56c7dbcf
Aman Kumar <amankumar141099@gmail.com>
2024-12-28 18:40:16 +0530

    FWA-50 - Config settings are getting reset on refresh

--- src/app/pages/admin/config/config.component.ts
+++ src/app/pages/admin/config/config.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { Level1HeaderComponent } from '../components/level-1-header/level-1-header.component';
 import { ConfigSettingsComponent } from './components/config-settings/config-settings.component';
+import { Level2HeaderComponent } from '../components/level-2-header/level-2-header.component';
 
 @Component({
   selector: 'app-config',
   standalone: true,
-  imports: [Level1HeaderComponent, ConfigSettingsComponent],
+  imports: [Level2HeaderComponent, ConfigSettingsComponent],
   templateUrl: './config.component.html',
   styleUrl: './config.component.scss',
 })
 export class ConfigComponent {
   headerText: string = 'Configuration';
-  buttonText: string = 'Add Doc';
-  childLevelLink: string = 'add-document';
 }
