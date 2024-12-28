8fe8a9b41301c4def1eda2f4727ae863a3536920
Aman Kumar <amankumar141099@gmail.com>
2024-12-28 17:52:57 +0530

    FWA-49 - UI refactoring

--- src/app/pages/admin/config/components/config-settings/config-settings.component.ts
+++ src/app/pages/admin/config/components/config-settings/config-settings.component.ts
@@ -5,26 +5,33 @@ import {
   toggleAdminHeaderVersion,
   toggleHeaderVersion,
 } from '../../../../../state/config/config.actions';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { selectHeaderConfig } from '../../../../../state/config/config.selectors';
 
 @Component({
   selector: 'app-config-settings',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, CommonModule, FormsModule],
   templateUrl: './config-settings.component.html',
   styleUrl: './config-settings.component.scss',
 })
 export class ConfigSettingsComponent {
-  constructor(private store: Store) {}
+  isNewHomeHeader$: Observable<boolean> = this.store
+    .select(selectHeaderConfig)
+    .pipe(map((headerConfig) => headerConfig.newHeader));
+  isNewAdminHeader$: Observable<boolean> = this.store
+    .select(selectHeaderConfig)
+    .pipe(map((headerConfig) => headerConfig.newAdminHeader));
 
-  ngOnInit() {}
+  constructor(private store: Store) {}
 
   toggleHomeHeader(checked: boolean) {
     this.store.dispatch(toggleHeaderVersion({ newHeader: checked }));
-    console.log(checked);
   }
 
   toggleAdminHeader(checked: boolean) {
     this.store.dispatch(toggleAdminHeaderVersion({ newAdminHeader: checked }));
-    console.log(checked);
   }
 }
