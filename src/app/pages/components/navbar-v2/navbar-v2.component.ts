06e5b5881417b655ca8b811286a6c38b9dcaf832
Aman Kumar <amankumar141099@gmail.com>
2024-12-28 15:20:02 +0530

    FWA-49 - UI refactoring

--- src/app/pages/components/navbar-v2/navbar-v2.component.ts
+++ src/app/pages/components/navbar-v2/navbar-v2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,6 +12,14 @@ import {
   selectAuthUser,
 } from '../../../state/auth/auth.selectors';
 import { CommonModule } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Project } from '../../../interface/project';
+import {
+  selectProjects,
+  selectIsProjectLoaded,
+} from '../../../state/project/project.selectors';
+import { loadProjects } from '../../../state/project/project.actions';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar-v2',
@@ -22,23 +30,45 @@ import { CommonModule } from '@angular/common';
     MatButtonModule,
     RouterModule,
     CommonModule,
+    MatMenuModule,
   ],
   templateUrl: './navbar-v2.component.html',
   styleUrl: './navbar-v2.component.scss',
 })
 export class NavbarV2Component {
+  @Output() toggleSidenav = new EventEmitter<void>();
   authLoaded$: Observable<boolean> = this.store.select(selectAuthLoaded);
   authUser$: Observable<User | null> = this.store.select(selectAuthUser);
+  projects$: Observable<Project[]> = this.store.select(selectProjects);
+  projectLoaded$: Observable<boolean> = this.store.select(
+    selectIsProjectLoaded
+  );
+  smallScreen: boolean = false;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit() {
     this.authLoaded$.subscribe((loaded) => {
       if (!loaded) this.store.dispatch(loadAuthUser());
     });
+    this.projectLoaded$.subscribe((loaded) => {
+      if (!loaded) this.store.dispatch(loadProjects());
+    });
+    this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.XSmall])
+      .subscribe((result) => {
+        this.smallScreen = result.matches;
+      });
   }
 
   isUserLoggedIn(): Observable<boolean> {
     return this.authUser$.pipe(map((user) => user !== null));
+  }
+
+  toggleMenu() {
+    this.toggleSidenav.emit();
   }
 }
