d8f425deef1f7497334fae2a4855da40d0e494ca
Ankit Kumar <ankitkumar0103@outlook.com>
2024-12-29 16:05:31 +0530

    some more styles

--- src/app/pages/home/landing/components/updates/updates.component.ts
+++ src/app/pages/home/landing/components/updates/updates.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -35,6 +35,9 @@ export class UpdatesComponent {
   postLoaded$: Observable<boolean> = this.store.select(selectPostLoaded);
   loading$: Observable<boolean> = this.store.select(selectPostLoading);
 
+  selectedPost: Post | null = null;
+  isModalVisible = false;
+
   constructor(private store: Store) {}
 
   ngOnInit() {
@@ -43,7 +46,14 @@ export class UpdatesComponent {
     });
   }
 
-  viewPost(id: number) {
-    this.store.dispatch(openViewDialog({ postId: id }));
+  viewPost(post: Post) {
+    //this.store.dispatch(openViewDialog({ postId: id }));
+    this.selectedPost = post;
+    this.isModalVisible = true;
+  }
+
+  closeModal() {
+    this.selectedPost = null;
+    this.isModalVisible = false;
   }
 }
