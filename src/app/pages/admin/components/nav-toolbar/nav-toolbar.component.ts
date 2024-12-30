4255edb00e3439a4a5776b09a38ac39fe3e55650
Aman Kumar <amankumar141099@gmail.com>
2024-12-30 17:25:06 +0530

    FWA-63 - Add a new user friendly navigation in Admin Console

---
+++ src/app/pages/admin/components/nav-toolbar/nav-toolbar.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { DisplayTextHeadComponent } from '../../../components/display-text-head/display-text-head.component';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-nav-toolbar',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    CommonModule,
+    MatIconModule,
+    RouterModule,
+    DisplayTextHeadComponent,
+    MatDividerModule,
+  ],
+  templateUrl: './nav-toolbar.component.html',
+  styleUrl: './nav-toolbar.component.scss',
+})
+export class NavToolbarComponent {
+  @Input() headerText: string = 'Projects';
+  @Input() buttonText: string = '';
+  @Input() childLevelLink: string = '';
+
+  hasChild(): boolean {
+    return this.childLevelLink !== '';
+  }
+
+  isChild(): boolean {
+    return true;
+  }
+}
