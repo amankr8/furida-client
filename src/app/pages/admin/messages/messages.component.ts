df5c638fda2b37cc3fb1030d956d2cedae249ac3
Aman Kumar <amankumar141099@gmail.com>
2024-12-30 17:33:55 +0530

    FWA-63 - Add a new user friendly navigation in Admin Console

--- src/app/pages/admin/messages/messages.component.ts
+++ src/app/pages/admin/messages/messages.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { Level1HeaderComponent } from '../components/level-1-header/level-1-header.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { NavToolbarComponent } from '../components/nav-toolbar/nav-toolbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [Level1HeaderComponent, CardsComponent],
+  imports: [
+    Level1HeaderComponent,
+    CardsComponent,
+    NavToolbarComponent,
+    CommonModule,
+  ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss',
 })
 export class MessagesComponent {
+  // To be removed in v2.2.0
+  newNavToolbar: boolean = true;
+
   headerText: string = 'Inbox';
   buttonText: string = 'Archive Inbox';
   childLevelLink: string = 'read-messages';
--- src/app/pages/admin/posts/posts.component.ts
+++ src/app/pages/admin/posts/posts.component.ts
@@ -1,15 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { Level1HeaderComponent } from '../components/level-1-header/level-1-header.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { CommonModule } from '@angular/common';
+import { NavToolbarComponent } from '../components/nav-toolbar/nav-toolbar.component';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [Level1HeaderComponent, CardsComponent],
+  imports: [
+    Level1HeaderComponent,
+    CardsComponent,
+    CommonModule,
+    NavToolbarComponent,
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
 })
 export class PostsComponent {
+  // To be removed in v2.2.0
+  newNavToolbar: boolean = true;
+
   headerText: string = 'Your Posts';
   buttonText: string = 'Create Post';
   childLevelLink: string = 'create-post';
