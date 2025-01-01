7c520c9276946539cb1e388f98b34f4ce6b70bcb
Aman Kumar <amankumar141099@gmail.com>
2025-01-01 12:10:31 +0530

    FWA-67 - Add table view for data in Admin Console

--- src/app/pages/admin/projects/projects.component.ts
+++ src/app/pages/admin/projects/projects.component.ts
@@ -4,13 +4,22 @@ import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { loadProjects } from '../../../state/project/project.actions';
-import { selectProjectLoaded } from '../../../state/project/project.selectors';
+import {
+  selectProjectLoaded,
+  selectProjectLoading,
+} from '../../../state/project/project.selectors';
 import { ProjectTableComponent } from './components/project-table/project-table.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [NavToolbarComponent, CommonModule, ProjectTableComponent],
+  imports: [
+    NavToolbarComponent,
+    CommonModule,
+    ProjectTableComponent,
+    MatProgressBarModule,
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
@@ -20,6 +29,7 @@ export class ProjectsComponent {
   childLevelLink: string = 'add-project';
 
   projectLoaded: Observable<boolean> = this.store.select(selectProjectLoaded);
+  loading$: Observable<boolean> = this.store.select(selectProjectLoading);
 
   constructor(private store: Store) {}
 
