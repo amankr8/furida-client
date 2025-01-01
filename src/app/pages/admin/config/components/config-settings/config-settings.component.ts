f175a454375150a47caa3d832b73a29241ead927
Aman Kumar <amankumar141099@gmail.com>
2025-01-01 21:15:14 +0530

    FWA-70 - Enhance table view for data in Admin view (#53)
    FWA-70 - Enhance table view for data in Admin view

--- src/app/pages/admin/config/components/config-settings/config-settings.component.ts
+++ src/app/pages/admin/config/components/config-settings/config-settings.component.ts
@@ -9,11 +9,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Observable, map } from 'rxjs';
 import { selectConfig } from '../../../../../state/config/config.selectors';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-config-settings',
   standalone: true,
-  imports: [MatSlideToggleModule, CommonModule, FormsModule],
+  imports: [MatSlideToggleModule, CommonModule, FormsModule, MatTooltipModule],
   templateUrl: './config-settings.component.html',
   styleUrl: './config-settings.component.scss',
 })
@@ -24,8 +26,20 @@ export class ConfigSettingsComponent {
   isBoolSetting2$: Observable<boolean> = this.store
     .select(selectConfig)
     .pipe(map((config) => config.boolSetting2));
+  isMobile: boolean = false;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMobile = result.matches;
+      });
+  }
 
   toggleHomeHeader(checked: boolean) {
     this.store.dispatch(toggleBoolSetting1({ boolSetting1: checked }));
