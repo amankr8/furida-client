bda573bf99a0a064e19f7907a4fb4bf57ede1249
Aman Kumar <amankumar141099@gmail.com>
2025-01-01 12:19:00 +0530

    FWA-67 - Add table view for data in Admin Console

--- src/app/pages/admin/projects/projects.component.ts
+++ src/app/pages/admin/projects/projects.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavToolbarComponent } from '../components/nav-toolbar/nav-toolbar.component';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { loadProjects } from '../../../state/project/project.actions';
 import {
   selectProjectLoaded,
@@ -10,6 +10,8 @@ import {
 } from '../../../state/project/project.selectors';
 import { ProjectTableComponent } from './components/project-table/project-table.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { selectConfig } from '../../../state/config/config.selectors';
+import { ProjectCardsComponent } from './components/project-cards/project-cards.component';
 
 @Component({
   selector: 'app-projects',
@@ -19,6 +21,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     CommonModule,
     ProjectTableComponent,
     MatProgressBarModule,
+    ProjectCardsComponent,
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
@@ -28,6 +31,9 @@ export class ProjectsComponent {
   buttonText: string = 'Add Project';
   childLevelLink: string = 'add-project';
 
+  tableView$: Observable<boolean> = this.store
+    .select(selectConfig)
+    .pipe(map((config) => config.boolSetting1));
   projectLoaded: Observable<boolean> = this.store.select(selectProjectLoaded);
   loading$: Observable<boolean> = this.store.select(selectProjectLoading);
 
