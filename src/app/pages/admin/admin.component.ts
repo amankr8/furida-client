7c8354e23e460706ef39ab73144453dceffc7534
Aman Kumar <amankumar141099@gmail.com>
2025-01-04 01:07:46 +0530

    FWA-73 - Implement sidenav for Admin screen

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -2,12 +2,13 @@ import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { loadAuthUser } from '../../state/auth/auth.actions';
 import { selectAuthLoaded } from '../../state/auth/auth.selectors';
 import { NavbarV2Component } from '../components/navbar-v2/navbar-v2.component';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { AdminSideMenuComponent } from './components/admin-side-menu/admin-side-menu.component';
+import { BreakpointService } from '../../service/breakpoint/breakpoint.service';
 
 @Component({
   selector: 'app-admin',
@@ -25,8 +26,12 @@ import { AdminSideMenuComponent } from './components/admin-side-menu/admin-side-
 export class AdminComponent {
   @ViewChild('sideDrawer') sideDrawer!: MatDrawer;
   authLoaded$: Observable<boolean> = this.store.select(selectAuthLoaded);
+  smallScreen$: Observable<boolean> = this.breakpointService.isSmallScreen();
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private breakpointService: BreakpointService
+  ) {}
 
   ngOnInit() {
     this.authLoaded$.subscribe((loaded) => {
