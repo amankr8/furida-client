c622715bade2cdae0094225cbfd7bc78f6ab32b5
Aman Kumar <amankumar141099@gmail.com>
2025-01-07 20:26:54 +0530

    Auth user doesn't load on relogging after expiry until a refesh

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { map, Observable } from 'rxjs';
-import { selectAuthLoaded } from '../../state/auth/auth.selectors';
 import { NavbarV2Component } from '../components/navbar-v2/navbar-v2.component';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { AdminSideMenuComponent } from './components/admin-side-menu/admin-side-menu.component';
 import { BreakpointService } from '../../service/breakpoint/breakpoint.service';
 import { toggleBoolSetting1 } from '../../state/config/config.actions';
+import { selectAuthLoaded } from '../../state/auth/auth.selectors';
+import { loadAuthUser } from '../../state/auth/auth.actions';
 
 @Component({
   selector: 'app-admin',
@@ -41,6 +42,12 @@ export class AdminComponent {
     this.breakpointService.isSmallerScreen().subscribe((small) => {
       if (small)
         this.store.dispatch(toggleBoolSetting1({ boolSetting1: false }));
+    });
+  }
+
+  ngOnInit() {
+    this.authLoaded$.subscribe((loaded) => {
+      if (!loaded) this.store.dispatch(loadAuthUser());
     });
   }
 
