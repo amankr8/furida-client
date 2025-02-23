63579d9ebdfa97ef99db75d2c8183fc4976e114c
amankr8 <amankumar141099@gmail.com>
2025-02-23 20:10:22 +0530

    session expired handled for server requests

--- src/app/shared/config/jwt.interceptor.ts
+++ src/app/shared/config/jwt.interceptor.ts
@@ -1,37 +1,23 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService } from '../../service/auth/auth.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const jwtHelper = inject(JwtHelperService);
-  const router = inject(Router);
   const authService = inject(AuthService);
-  const snackBar = inject(MatSnackBar);
-
   const token = authService.getAuthToken();
 
   if (token) {
-    if (jwtHelper.isTokenExpired(token)) {
-      snackBar.open(
-        'Unable to send request! Your session has expired.',
-        'Dismiss',
-        {
-          duration: 3000,
-        }
-      );
-      router.navigate(['/login']);
+    if (!jwtHelper.isTokenExpired(token)) {
+      // Attach Authorization header if token is valid
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return next(req);
     }
-
-    // Attach Authorization header if token is valid
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
   }
 
   return next(req);
